e74145cedda596e53c8458b5a7a2a5d91e5200ae
GANZIII <fullsun66@korea.ac.kr>
2022-07-18 14:50:09 +0900

    qkqh

--- get_next_line.c
+++ get_next_line.c
@@ -6,14 +6,14 @@
 /*   By: jijoo <jijoo@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/09 17:51:45 by jijoo             #+#    #+#             */
-/*   Updated: 2022/07/09 21:56:23 by jijoo            ###   ########.fr       */
+/*   Updated: 2022/07/18 14:46:11 by jijoo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 #include <stdio.h>
 #include <fcntl.h>
-#define BUFFER_SIZE 3
+#define BUFFER_SIZE 7
 
 unsigned long	ft_strlen(const char *str)
 {
@@ -68,47 +68,59 @@ unsigned long   ft_strcat(char *dest, const char *src)
 	dest[len_dest + i] = 0;
 	return (len_dest + len_src);
 }
+
+char	*end(int s, char *b, char *big)
+{
+	char	*little;
+
+	*(b + s) = 0;
+	little = ft_strdup(b);
+	ft_strcat(big, little);
+	//(little);
+	return ((char *)big);
+}
+
 char    *get_next_line(int fd)
 {
-    char    *line;
-    char    buf[BUFFER_SIZE];
-    int     size;
-    char    *little;
-    char    *big;
+    char    	buf[BUFFER_SIZE];
+    int     	size;
+    static char	*big;
+	int			k;
 
     size = 0;
     big = ft_strdup("");
-    while ((read(fd, buf + size, 1) == 1) && (*(buf + size) != '\n'))
-    {
-        size++;
-        if (size + 2 == BUFFER_SIZE)
-        {
-            *(buf + size) = 0;
-            little = ft_strdup(buf);
-            ft_strcat(big, little);
-            size = 0;
-        }
-    }
-    *(buf + size) = 0;
-    little = ft_strdup(buf);
-    ft_strcat(big, little);
-    ft_strcat(big, "\n");
-    line = big;
-    return (line);
+	while (1)
+	{
+		k = read(fd, buf + size, 1);
+		if (k == -1)
+			return (0);
+		else if (k == 0)
+			return (end(size, buf, big));
+   		else if (k == 1)
+    	{
+			if (*(buf + size) == '\n')
+				return (end(size + 1, buf, big));
+        	if ((++size) + 2 == BUFFER_SIZE)
+        	{
+            	big = end(size, buf, big);
+            	size = 0;
+        	}
+    	}
+	}
 }
 
 int main(void)
 {
-    int   fp;
-    fp = open("./hello.txt", O_RDONLY);
-    char    *ret1 = get_next_line(fp);
-    printf("ret : %s", ret1);
-    char    *ret2 = get_next_line(fp);
-    printf("ret : %s", ret2);
-    char    *ret3 = get_next_line(fp);
-    printf("ret : %s", ret3);
-    char    *ret4 = get_next_line(fp);
-    printf("ret : %s", ret4);
-    close(fp);
+	int fd = open("./hello.txt", O_RDONLY);
+
+	while (1)
+	{
+		char *line = get_next_line(1);
+		printf("%s", line);
+	}
+
+
+
+	close(fd);
     return (0);
 }
